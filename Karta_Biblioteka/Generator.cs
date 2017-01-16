d458522cc2d2e6bf0050d7277bd80f871687dd6f
Wojtek <Wojtek@WOJTEK-KOMPUTER>
2017-01-16 16:29:49 +0100

    Add project files.

---
+++ Karta_Biblioteka/Generator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Karta_Biblioteka
+{
+    public static class Generator
+    {
+        static Random rnd = new Random();
+
+
+
+        public static DateTime GenerateDate()
+        {
+
+            DateTime time = new DateTime(rnd.Next(2015, 2017), rnd.Next(1, 12), rnd.Next(1, 29));
+
+            return time;
+        }
+
+        public static string GeneratePostalCode()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(rnd.Next(10, 99));
+            sb.Append('-');
+            sb.Append(rnd.Next(100, 999));
+
+            return sb.ToString();
+        }
+
+        public static string GenerateNrDomu()
+        {
+            return rnd.Next(1, 99).ToString();
+        }
+        public static string GenerateNrM()
+        {
+            int nr = rnd.Next(1, 50);
+            if (nr > 10)
+            {
+                return null;
+            }
+            else
+            {
+                return nr.ToString();
+            }
+        }
+
+        public static string GenerateName()
+        {
+            string line;
+            StreamReader file = new StreamReader("Imiona.txt");
+            line = file.ReadLine();
+            //Console.WriteLine(line);
+            var nameArray = line.Split(',');
+            int nameArraySize = nameArray.Length;
+            return nameArray[rnd.Next(1, nameArraySize)].Trim();
+        }
+
+        public static string GenerateStreet()
+        {
+            int streetArraySize = 135;
+            var file = new StreamReader("Streets.txt");
+            var streetArray = new string[streetArraySize];
+            for (int i = 0; i < streetArraySize; i++)
+            {
+                streetArray[i] = file.ReadLine();
+            }
+
+            return streetArray[rnd.Next(1, streetArraySize)];
+        }
+
+        public static string GenerateCity()
+        {
+            string line;
+            var file = new StreamReader("Miasta.txt");
+            line = file.ReadLine();
+            var cityArray = line.Split(' ');
+            int cityArraySize = cityArray.Length;
+
+            return cityArray[rnd.Next(1, cityArraySize)];
+        }
+
+        public static string GeneratePhoneNumber()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < 9; i++)
+            {
+                sb.Append(rnd.Next(1, 9));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Karta_Biblioteka/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Karta_Biblioteka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+
+        void FillTable(SqlConnection conn)
+        {
+            try
+            {
+                conn.Open();
+                using (SqlCommand command = new SqlCommand(@"INSERT INTO Karta (Imię, Nazwisko, Miejscowość, [Kod pocztowy], Ulica, [Nr domu], [Nr mieszkania],[Nr kontaktowy], [Data wydania]) 
+                                                         OUTPUT INSERTED.ID VALUES (@imie, @nazwisko, @city, @kod, @ulica, @nrd, @nrm, @nrtel, @data)", conn))
+                {
+                    command.Parameters.AddWithValue("@imie", Generator.GenerateName());
+                    command.Parameters.AddWithValue("@nazwisko", Generator.GenerateName());
+                    command.Parameters.AddWithValue("@city", Generator.GenerateCity());
+                    command.Parameters.AddWithValue("@kod", Generator.GeneratePostalCode());
+                    command.Parameters.AddWithValue("@ulica", Generator.GenerateStreet());
+                    command.Parameters.AddWithValue("@nrd", Generator.GenerateNrDomu());
+                    string nrM = Generator.GenerateNrM();
+                    if (nrM == null)
+                    {
+                        command.Parameters.AddWithValue("@nrm", DBNull.Value);
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("@nrm", nrM);
+                    }
+                    command.Parameters.AddWithValue("@nrtel", Generator.GeneratePhoneNumber());
+                    command.Parameters.AddWithValue("@data", Generator.GenerateDate());
+                    command.ExecuteScalar();
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConn_Click(object sender, RoutedEventArgs e)
+        {
+            conn = new SqlConnection(tbConnString.Text);
+            btnAdd.IsEnabled = true;
+            try
+            {
+                conn.Open();
+                conn.Close();
+            }
+            catch (Exception exc)
+            {
+                tbConnError.Text = exc.Message;
+                throw;
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < int.Parse(tbQuantity.Text); i++)
+            {
+                FillTable(conn);
+            }
+        }
+
+        private void tbQuantity_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
